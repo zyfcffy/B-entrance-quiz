ccb7ad870fb1fd35c44c825f277c30c704112969
Xiaofeng Wang <xfun68@gmail.com>
2020-09-06 18:13:37 +0800

    generated from start.spring.com

---
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.capability.gtb.entrancequiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtbEntranceQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtbEntranceQuizApplication.class, args);
+	}
+
+}
